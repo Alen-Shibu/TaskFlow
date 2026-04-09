8fb61ac60f6f34da06db0a35a58ee0a3d582577e
Alen Shibu <your-email@example.com>
2026-04-09 08:46:54 +0530

    stores and cors setup

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,9 +4,15 @@ import authRoutes from './routes/auth.routes.js'
 import taskRoutes from './routes/task.routes.js'
 import {connectDB} from './lib/db.js'
 import cookieParser from 'cookie-parser'
+import cors from 'cors'
 
 const app = express()
 const PORT = process.env.PORT || 5000
+
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:5173', 
+  credentials: true,               
+}));
 
 app.use(express.json())
 app.use(cookieParser())
